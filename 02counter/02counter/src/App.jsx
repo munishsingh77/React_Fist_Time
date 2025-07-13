2af42308593790c3c9bb7b218a76ff6a6b96dfd5
munishsingh77 <you@example.com>
2025-07-13 09:33:34 +0530

    react vite push

---
+++ 02counter/02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(15)
+
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(prevCounter => prevCounter + 1)
+    }
+  }
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(prevCounter => prevCounter - 1)
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>
+        Add value {counter}
+      </button>
+      <br />
+      <button onClick={removeValue}>
+        Remove value {counter}
+      </button>
+      
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
